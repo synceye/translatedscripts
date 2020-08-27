c21e216ccd2dbd196016f73df9f8f54805f342d0
SyncEye <50116007+DamjanPetrovicki@users.noreply.github.com>
2020-08-27 18:12:57 +0200

    prijevod za sve skripte

---
+++ oruzarnica.lua
@@ -0,0 +1,13 @@
+Locales ['hr'] = {
+  ['buy_license'] = 'Kupi licencu za ozuzije?',
+  ['yes'] = '%s',
+  ['no'] = 'Ne',
+  ['weapon_bought'] = 'Kupljeno za $%s',
+  ['not_enough_black'] = 'Nemaš dovoljno prljavog novca',
+  ['not_enough'] = 'Nemaš dovoljno novaca',
+  ['already_owned'] = 'Već imas to oruzije!',
+  ['shop_menu_title'] = 'Oružarnica',
+  ['shop_menu_prompt'] = 'Pritisni ~INPUT_CONTEXT~ da otvoriš ~y~Oružarnicu~s~.',
+  ['shop_menu_item'] = '$%s',
+  ['map_blip'] = 'Oružarnica',
+}
---
+++ skin.lua
@@ -0,0 +1,6 @@
+Locales['hr'] = {
+  ['skin_menu'] = 'Skin Meni',
+  ['use_rotate_view'] = 'Koristi ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ i ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ da okreces kameru.',
+  ['skin'] = 'Promjeni Skin',
+  ['saveskin'] = 'Spremi skin kao file',
+}
