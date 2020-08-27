f43f4dc5969874d7be843340a18092e8f67d51c3
SyncEye <50116007+DamjanPetrovicki@users.noreply.github.com>
2020-08-27 20:04:14 +0200

    Add files via upload

---
+++ cjat.lua
@@ -0,0 +1,11 @@
+Locales['hr'] = {
+  ['ooc_prefix'] = 'OOC | %s',
+  ['twt_help'] = 'posalji tweet',
+  ['twt_prefix'] = '^0[^4Twitter^0] (^5@%s^0)',
+  ['me_help'] = 'osobne akcije',
+  ['me_prefix'] = 'Me | %s',
+  ['do_help'] = 'RP info',
+  ['do_prefix'] = 'Do | %s',
+  ['generic_argument_name'] = 'poruka',
+  ['generic_argument_help'] = 'poruka',
+}
