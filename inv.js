9482d25873ccbf1179363b841260b4b26c94bc20
SyncEye <50116007+DamjanPetrovicki@users.noreply.github.com>
2020-08-29 16:55:57 +0200

    Add files via upload

---
+++ inv.js
@@ -0,0 +1,5 @@
+var invLocale = new Object();
+invLocale.dropItem = "Makni";
+invLocale.useItem = "Koristi";
+invLocale.giveItem = "Daj";
+invLocale.secondInventoryNotAvailable = "Drugi inventar nije dostupan.";
