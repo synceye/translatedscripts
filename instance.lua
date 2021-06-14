dd9849c478eb95879fcc559e78f981e18e0763be
sync <50116007+synceye@users.noreply.github.com>
2021-06-14 21:33:51 +0200

    Delete instance.lua

--- instance.lua
+++
@@ -1,8 +0,0 @@
-Locales['hr'] = {
-  ['left_instance'] = 'Izašli ste s posjeda',
-  ['invite_expired'] = 'Poziv je ispario',
-  ['press_to_enter'] = 'Pritisni ~INPUT_CONTEXT~ da udes na posjed',
-  ['entered_instance'] = 'Ušao si na posjed',
-  ['entered_into'] = '%s je ušo na posjed',
-  ['left_out'] = '%s je izašao s posjeda',
-}
